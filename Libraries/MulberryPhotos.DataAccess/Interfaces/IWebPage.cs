56fab6f580688e47ecb49ad70630f67a5d90250a
Gary Walls <gary.a.walls@gmail.com>
2018-02-03 14:38:18 +0000

    Created DataAccess library and added base data access interfaces.

---
+++ Libraries/MulberryPhotos.DataAccess/Interfaces/IWebPage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MulberryPhotos.DataAccess.Interfaces
+{
+    public interface IWebPage
+    {
+        string Name { get; }
+
+        string Title { get; }
+
+        IMetaData MetaData { get; }
+
+        List<string> ImageList { get;  }
+
+        List<IWebSection> WebSections { get; }
+    }
+}
