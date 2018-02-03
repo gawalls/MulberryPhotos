d8bc6e9524664d2f85de850b5be6e4b1ff7a4d3f
Gary Walls <gary.a.walls@gmail.com>
2018-02-03 15:58:38 +0000

    amended interfaces

--- Libraries/MulberryPhotos.DataAccess/Interfaces/IWebPageContentProvider.cs
+++ Libraries/MulberryPhotos.DataAccess/Interfaces/IWebPageContentProvider.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace MulberryPhotos.DataAccess.Interfaces
 {
-    public interface IRepository
+    public interface IWebPageContentProvider
     {
         string GetWebPageName();
 
