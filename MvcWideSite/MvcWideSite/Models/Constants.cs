808ea8817c8e590b92aa59aa27e406fe95fb4833
Gary Walls <gary.walls@redletterdays.co.uk>
2018-02-06 09:51:10 +0000

    started front end

---
+++ MvcWideSite/MvcWideSite/Models/Constants.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using MulberryPhotos.DataAccess.Repositories;
+
+namespace MvcWideSite.Models
+{
+    public static class Constants
+    {
+        public static class Locations
+        {
+            public static string WebDataXmlFile
+            {
+                get
+                {
+                    //return HttpContext.Current.Server.MapPath(@"~/App_Data/WebsiteContent.xml");
+                    return HostingEnvironment.MapPath(@"~/App_Data/WebsiteContent.xml");
+                }
+            }
+        }
+
+        public static XmlWebPageRepository Repository
+        {
+            get
+            {
+                return new XmlWebPageRepository(Locations.WebDataXmlFile);
+            }
+        }
+    }
+}
