f0ada38df045f8f431e57c399b411d64acf59088
gary walls <gary.a.walls@gmail.com>
2018-02-22 16:36:37 +0000

    added bundle optimization

--- MvcWideSite/MvcWideSite/App_Start/BundleConfig.cs
+++ MvcWideSite/MvcWideSite/App_Start/BundleConfig.cs
@@ -1,4 +1,6 @@
-﻿using System.Web;
+﻿using System;
+using System.Configuration;
+using System.Web;
 using System.Web.Optimization;
 
 namespace MvcWideSite
@@ -8,24 +10,45 @@ namespace MvcWideSite
         // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+            var scriptsBundle = new ScriptBundle("~/bundles/jquery")
+                .Include("~/Scripts/jquery-{version}.js")
+                .Include("~/Scripts/jquery.validate*");
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+            var modernizerBundle = new ScriptBundle("~/bundles/modernizr")
+                .Include("~/Scripts/modernizr-*");
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+            var bootstrapBundle = new ScriptBundle("~/bundles/bootstrap")
+                .Include("~/Scripts/bootstrap.js")
+                .Include("~/Scripts/respond.js");
 
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            var cssBundle = new StyleBundle("~/Content/css")
+                 .Include("~/Content/bootstrap.css")
+                 .Include("~/Content/site.css");
+
+            scriptsBundle.Transforms.Add(new JsMinify());
+            modernizerBundle.Transforms.Add(new JsMinify());
+            bootstrapBundle.Transforms.Add(new JsMinify());
+            cssBundle.Transforms.Add(new CssMinify());
+            
+            bundles.Add(scriptsBundle);
+            bundles.Add(modernizerBundle);
+            bundles.Add(bootstrapBundle);
+            bundles.Add(cssBundle);            
+            BundleTable.EnableOptimizations = GetOptimizationSetting();
+        }
+
+        private static bool GetOptimizationSetting()
+        {
+            try
+            {
+                return Convert.ToBoolean(ConfigurationManager.AppSettings["Optimize"]);
+            }
+            catch
+            {
+                return false;
+            }            
         }
     }
 }
